d547a20924b67feec77b41214d00d71722f0ce6b
Hugo Vila <git@hugovila.nom.es>
2014-07-29 20:15:01 +0200

    Testing css styles and assets pipeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  layout "post"
+
   # GET /posts
   # GET /posts.json
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,16 @@
 
 require 'securerandom'
 
-75.times do
-  Post.create(
-    :title => "My Post #{SecureRandom.hex(2)}",
-    :author => SecureRandom.hex(6),
-    :body => SecureRandom.hex(32)
-  )
+# 75.times do
+#   Post.create(
+#     :title => "My Post #{SecureRandom.hex(2)}",
+#     :author => SecureRandom.hex(6),
+#     :body => SecureRandom.hex(32)
+#   )
+# end
+
+32.times do
+  Post.create(:title => "Hand post #{SecureRandom.hex(2)}", :author => "Juan", :body => "Una mañana, tras un sueño intranquilo, Gregorio.")
+  Post.create(:title => "Handy post #{SecureRandom.hex(2)}", :author => "Hugo", :body => "Y, viéndole don Quijote de aquella manera, ...")
+  Post.create(:title => "Hand post #{SecureRandom.hex(2)}", :author => "Pedro", :body => "Muy lejos, más allá de las montañas de palabras, ...")
 end
