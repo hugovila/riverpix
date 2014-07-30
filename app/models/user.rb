8b6e323e67e365df8335dbf98bf241ff4951987f
Hugo Vila <git@hugovila.nom.es>
2014-07-30 17:16:37 +0200

    Updating user model to add avatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   belongs_to :group
   has_many :posts
   has_many :likes
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
