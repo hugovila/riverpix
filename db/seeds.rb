0ac657757ba83e8f293f3893747950d5f5edf158
Hugo Vila <git@hugovila.nom.es>
2014-08-08 00:55:01 +0200

    Added 75 rows to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ require 'securerandom'
 #   )
 # end
 
-32.times do
-  Post.create(:title => "Hand post #{SecureRandom.hex(2)}", :author => "Juan", :body => "Una mañana, tras un sueño intranquilo, Gregorio.")
-  Post.create(:title => "Handy post #{SecureRandom.hex(2)}", :author => "Hugo", :body => "Y, viéndole don Quijote de aquella manera, ...")
-  Post.create(:title => "Hand post #{SecureRandom.hex(2)}", :author => "Pedro", :body => "Muy lejos, más allá de las montañas de palabras, ...")
+75.times do
+  Post.create(:user_id => 1, :title => "Hand post #{SecureRandom.hex(2)}", :author => "Juan", :body => "Una mañana, tras un sueño intranquilo, Gregorio Samsa se despertó convertido en un monstruoso insecto. Estaba echado de espaldas sobre un duro caparazón y, al alzar la cabeza, vio su vientre convexo y oscuro, surcado por curvadas callosidades, sobre el que ...")
+  Post.create(:user_id => 1, :title => "Handy post #{SecureRandom.hex(2)}", :author => "Hugo", :body => "Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de s...")
+  Post.create(:user_id => 1, :title => "Hand post #{SecureRandom.hex(2)}", :author => "Pedro", :body => "Muy lejos, más allá ide las montañas de palabras, alejados de los países de las vocales y las consonantes, viven los textos simulados. Viven aislados en casas de letras, en la costa de la semántica, un gran océano de lenguas. Un riachuelo llamado Pons fluye ...")
 end
